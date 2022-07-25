ed431e6411fe72c5083f3d8e9e3a70a300d228fa
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2022-07-25 08:02:24 -0300

    carousel row

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,7 @@
 import { ResponsesData } from "../typings";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import Thumbnail from "./Thumbnail";
+import { useRef, useState } from "react";
 
 interface RowProps {
   title: string;
@@ -8,6 +9,24 @@ interface RowProps {
 }
 
 function Row({ title, movies }: RowProps) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className=" h-40 space-y-0.5 md:space-y-2">
       <h2
@@ -18,18 +37,25 @@ function Row({ title, movies }: RowProps) {
       </h2>
       <div className="group relative md:-ml-2">
         <AiOutlineLeft
-          className="z-40 absolute top-0 bottom-0 left-2 m-auto h-9 w-9 
-        cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          onClick={() => handleClick("left")}
+          className={`${
+            !isMoved && "hidden"
+          } z-40 absolute top-0 bottom-0 left-2 m-auto h-9 w-9 
+          cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
         />
 
-        <div className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 
-        scrollbar-hide">
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 
+        scrollbar-hide"
+        >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
 
         <AiOutlineRight
+          onClick={() => handleClick("right")}
           className="z-40 absolute top-0 bottom-0 right-2 m-auto h-9 w-9 
         cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
         />
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage<Responses> = ({ responses }: Responses) => {
     romanceMovies,
   } = responses;
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
       </Head>
